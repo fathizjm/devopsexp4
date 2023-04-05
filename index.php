faf4205ad3d778924049d0013ade801d4949c6e2
fathizjm <fsamreen14@gmail.com>
2023-04-05 23:43:11 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+	<style>
+		body {
+			background-color: #f2f2f2;
+			color: #333;
+			font-family: Arial, sans-serif;
+			font-size: 16px;
+			line-height: 1.5;
+			margin: 0;
+			padding: 0;
+		}
+		
+		h1 {
+			font-size: 48px;
+			margin: 50px 0 20px;
+		}
+		
+		p {
+			font-size: 24px;
+			margin: 0 0 20px;
+		}
+		
+		.navbar {
+			background-color: #333;
+			overflow: hidden;
+			position: fixed;
+			top: 0;
+			width: 100%;
+		}
+
+		.navbar a {
+			float: left;
+			color: white;
+			text-align: center;
+			padding: 14px 16px;
+			text-decoration: none;
+			font-size: 17px;
+		}
+
+		.navbar a:hover {
+			background-color: #ddd;
+			color: black;
+		}
+		
+		.separator {
+			background-color: #ccc;
+			height: 10px;
+			width: 100%;
+		}
+	</style>
+</head>
+<body>
+	<div class="navbar">
+		<a href="#">Home</a>
+		<a href="#">About</a>
+		<a href="#">Contact</a>
+	</div>
+	<div class="separator"></div>
+	<h1 style="text-align:center;">Welcome to my website!</h1>
+	<p style="text-align:center;">My name is Fathima.</p>
+	<div class="separator"></div>
+</body>
+</html>
